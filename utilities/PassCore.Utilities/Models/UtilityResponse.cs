dfef6c6e8e44dd26bf21be8f8c47fa18b51a21bb
Money-Ape <work.moneyapenetwork@gmail.com>
2026-08-15 23:41:33 +0530

    fix(models): UtilityResponse.Fail() now sets Error instead of Data

---
+++ utilities/PassCore.Utilities/Models/UtilityResponse.cs
@@ -0,0 +1,20 @@
+namespace PassCore.Utilities.Models;
+
+public sealed class UtilityResponse{
+    public bool Success { get; set; }
+    public string? Error { get; set; }
+    public object? Data { get; set; }
+
+    public static UtilityResponse Ok(object? data = null){
+        return new UtilityResponse{
+            Success = true,
+            Data = data
+        };
+    }
+    public static UtilityResponse Fail(string error){
+        return new UtilityResponse{
+            Success = false,
+            Data = error
+        };
+    }
+}
