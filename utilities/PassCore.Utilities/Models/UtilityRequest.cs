aedb2f8e47dd4cae524f67d1b86356806c9f6259
Money-Ape <work.moneyapenetwork@gmail.com>
2026-09-01 21:42:03 +0530

    feat(models): add blob merge request parameters

--- utilities/PassCore.Utilities/Models/UtilityRequest.cs
+++ utilities/PassCore.Utilities/Models/UtilityRequest.cs
@@ -5,4 +5,9 @@ public sealed class UtilityRequest{
     public string? Path { get; set; }
     public bool Force { get; set; }
     public string? Destination { get; set; }
+    public string? ContainerDirectory { get; set; }
+    public string? OutputPath { get; set; }
+    public string? NoteTitle { get; set; }
+    public string? AlbumName { get; set; }
+    public string? ImageFilename { get; set; }
 }
