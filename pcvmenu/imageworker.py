439ac3e9d258bfa9f2af183d1abdc76fd628eee0
Money-Ape <work.moneyapenetwork@gmail.com>
2026-09-01 21:41:45 +0530

    refactor(imageworker): pass utility to image loader

--- pcvmenu/imageworker.py
+++ pcvmenu/imageworker.py
@@ -4,14 +4,15 @@ from pcvmenu.images import load_preview
 class ImageLoader(QObject):
     finished = Signal(str, str, object)
 
-    def __init__(self, vault_key):
+    def __init__(self, utility, vault_key):
         super().__init__()
+        self.utility = utility
         self.vault_key = vault_key
 
     @Slot(str, str)
     def load(self, filename, album_name):
         try:
-            pix = load_preview(self.vault_key, filename, album_name)
+            pix = load_preview(self.utility, self.vault_key, filename, album_name)
 
         except Exception as e:
             print(e)
