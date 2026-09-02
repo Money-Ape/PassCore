68bd9be211799a694bdc60c405795f8a346ff9e9
Money-Ape <work.moneyapenetwork@gmail.com>
2026-09-02 23:50:43 +0530

    feat(version): add version config file

---
+++ version.py
@@ -0,0 +1,7 @@
+__majorversion__ = 0
+__minorversion__ = 7
+__patch_release__ = 0
+
+__release_identifier__ = "beta"
+
+__version__ = f"{__majorversion__}.{__minorversion__}.{__patch_release__}-{__release_identifier__}"
